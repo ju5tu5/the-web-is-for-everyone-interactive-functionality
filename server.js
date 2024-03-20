de683f2d0f36edc4a15b4621d623537d683ca080
Justus Sturkenboom <justus@ju5tu5.nl>
2024-03-20 17:10:09 +0100

    Voorbeeld POST/PATCH route uitgewerkt (voor RedPers)

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 import express from 'express'
 import fetchJson from './helpers/fetch-json.js'
 
-// It's an array of objects containing slug and like#, eg. [{ slug: 'a', likes: 42}]
-let likes = []
+const redpers_url = 'https://redpers.nl/wp-json/wp/v2/posts'
+const directus_url = 'https://fdnd-agency.directus.app/items/redpers_shares'
 
 const app = express()
 app.set('view engine', 'ejs')
@@ -12,37 +12,47 @@ app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (request, response) => {
-  fetchJson('https://redpers.nl/wp-json/wp/v2/posts').then((data) => {
-    fetchJson('https://fdnd-agency.directus.app/items/redpers_shares').then((share_data) => {
-      response.render('index', { articles: data, shares: share_data.data })
+  fetchJson(redpers_url).then((data) => {
+    fetchJson(directus_url).then((shares) => {
+      // Map de data array uit wordpress, map is een soort 'loop' structuur voor arrays
+      data.map((article) =>
+        // Gebruik de Object.assign() functie om het aantal shares toe te voegen op het article
+        // NB: Object.assign past het 'article' object
+        Object.assign(article, {
+          // Zoek in shares naar article.slug, koppel het aantal shares of 0 als het niet bestaat
+          shares: shares.data.find(({ slug }) => slug == article.slug)?.shares || 0,
+        })
+      )
+      response.render('index', { articles: data })
     })
   })
 })
 
-app.post('/article/:slug', (request, response) => {
-  // Zoek in de array de likes voor het huidige artikel
-  let huidig = likes.find((like) => {
-    return like.slug == request.params.slug
-  })
+// Failsafe voor als iemand /article intypt zonder slug...
+app.get('/article', (request, response) => {
+  response.redirect(301, '/')
+})
 
-  // Als het huidige artikel nog niet bestaat maken we likes aan
-  if (huidig == undefined) {
-    likes.push({
-      slug: request.params.slug,
-      likes: 1,
+app.post('/article/:slug', (request, response) => {
+  fetchJson(`${directus_url}?filter[slug][_eq]=${request.params.slug}`).then(({ data }) => {
+    // Doe een PATCH op directus, stuur de id mee als die er is.
+    fetchJson(`${directus_url}/${data[0]?.id ? data[0].id : ''}`, {
+      method: data[0]?.id ? 'PATCH' : 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        slug: request.params.slug,
+        shares: data.length > 0 ? data[0].shares + 1 : 1,
+      }),
+    }).then((result) => {
+      console.log(result)
     })
-    // Als het al wel bestaat tellen we er een like bij op
-  } else {
-    huidig.likes++
-  }
+  })
   response.redirect(301, `/article/${request.params.slug}`)
 })
 
 app.get('/article/:slug', (request, response) => {
-  fetchJson(`https://redpers.nl/wp-json/wp/v2/posts/?slug=${request.params.slug}`).then((data) => {
-    // Zoek naar de likes voor deze slug en voeg ze toe aan de opgehaalde API data
-    let current_likes = likes.find((like) => like.slug == request.params.slug)
-    data[0].likes = current_likes?.likes || 0
+  fetchJson(`${redpers_url}?slug=${request.params.slug}`).then((data) => {
+    // fetch de #shares
     response.render('article', { article: data[0] })
   })
 })
